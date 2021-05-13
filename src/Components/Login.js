b15fab1c8774ad3ae4267b140c4368b659583b7e
Navian <primeguyy@gmail.com>
2021-05-13 13:15:28 +0000

    hero illustration added to login screen

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -15,7 +15,22 @@ const Login = (props) => {
       </Nav>
       <Section>
         <Hero>
-          <h1>Welcome to your professional community</h1>
+          <div>
+            <h1>Welcome to your professional community</h1>
+            <Form>
+              <GoogleSignIn>
+                <img src="/images/google.svg" alt="sign in with google" />
+                Sign in with Google
+              </GoogleSignIn>
+            </Form>
+          </div>
+          <div>
+            <img
+              src="/images/login-illustration.svg"
+              alt="hero illustration"
+              className="hero-illustration"
+            />
+          </div>
         </Hero>
       </Section>
     </Container>
@@ -68,7 +83,7 @@ const Join = styled.a`
 const SignIn = styled.a`
   background-color: transparent;
   border-radius: 24px;
-  box-shadow: inset 0 0 0 2px #0a66c2;
+  box-shadow: inset 0 0 0 1px #0a66c2;
   color: #0a66c2;
   cursor: pointer;
   font-size: 16px;
@@ -86,15 +101,15 @@ const SignIn = styled.a`
 `;
 
 const Section = styled.section`
-  /* border: 1px solid red; */
+  /* border: 2px solid red; */
   display: flex;
   align-content: flex-start;
   align-items: center;
   flex-wrap: wrap;
   min-height: 700px;
-  margin: 0;
+  margin: auto;
   max-width: 1128px;
-  padding: 100px 0 160px;
+  padding: 70px 0 120px;
   position: relative;
   width: 100%;
 
@@ -104,6 +119,8 @@ const Section = styled.section`
 `;
 
 const Hero = styled.div`
+  display: flex;
+  align-items: center;
   width: 100%;
 
   h1 {
@@ -112,7 +129,8 @@ const Hero = styled.div`
     font-weight: 300;
     line-height: 70px;
     padding-bottom: 0;
-    width: 55%;
+    margin-bottom: 1rem;
+    width: 60%;
 
     @media (max-width: 768px) {
       font-size: 20px;
@@ -121,6 +139,36 @@ const Hero = styled.div`
       width: 100%;
     }
   }
+
+  img.hero-illustration {
+    /* border: 2px solid red; */
+    position: absolute;
+    bottom: 230px;
+    right: -150px;
+    /* right: -2px; */
+    height: 670px;
+    width: 700px;
+    z-index: -1;
+
+    @media (max-width: 768px) {
+      top: 230px;
+      bottom: -2px;
+      width: inherit;
+      margin: auto;
+      height: initial;
+      position: initial;
+      z-index: 0;
+    }
+  }
+
+  @media (max-width: 768px) {
+    flex-direction: column;
+    padding: 0 10px;
+  }
 `;
+
+const Form = styled.div``;
+
+const GoogleSignIn = styled.button``;
 
 export default Login;
